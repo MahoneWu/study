636b414bcc13b8d4474b91a36a26f9b3b4a87cfc
Mahone <2070509107@qq.com>
2018-10-24 19:28:44 +0800

    数组

---
+++ leetcode/src/main/java/com/mouse/data/structure/array/ArrayDemo.java
@@ -0,0 +1,22 @@
+package com.mouse.data.structure.array;
+
+/**
+ * Created by Mahone Wu on 2018/10/24.
+ */
+public class ArrayDemo {
+
+
+    public static void main(String[] args) {
+
+        GeneriArray array = new GeneriArray(5);
+        array.add(0, 10);
+        array.add(1, 10);
+        array.add(2, 10);
+        array.add(3, 10);
+        array.add(4, 10);
+        System.out.println(array);
+
+
+    }
+
+}
